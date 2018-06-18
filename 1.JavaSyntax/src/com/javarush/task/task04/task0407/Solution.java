f50f154908bc558478b01001d38b40381069d23b
st_nekto <deform@tut.by>
2018-06-18 16:37:35 +0300

    the solution0407

--- 1.JavaSyntax/src/com/javarush/task/task04/task0407/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0407/Solution.java
@@ -7,10 +7,10 @@ package com.javarush.task.task04.task0407;
 public class Solution {
     public static void main(String[] args) {
         Cat cat1 = new Cat();
-        //напишите тут ваш код
+        Cat.count++;//напишите тут ваш код
 
         Cat cat2 = new Cat();
-        //напишите тут ваш код
+        Cat.count++;//напишите тут ваш код
 
         System.out.println("Cats count is " + Cat.count);
     }
