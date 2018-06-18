db2d32774625de5b62b50306da032dffc9a953fd
st_nekto <deform@tut.by>
2018-06-18 17:04:05 +0300

    the solution0409

--- 1.JavaSyntax/src/com/javarush/task/task04/task0409/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0409/Solution.java
@@ -11,7 +11,17 @@ public class Solution {
     }
 
     public static void closeToTen(int a, int b) {
-        //напишите тут ваш код
+        int c = 10 - a;
+        int d = 10 - b;
+        if(Solution.abs(c) < Solution.abs(d)){
+            System.out.println(a);
+        }
+        else if(Solution.abs(d) < Solution.abs(c)){
+            System.out.println(b);
+        }//напишите тут ваш код
+        else{
+            System.out.println(a);
+        }
 
     }
 
