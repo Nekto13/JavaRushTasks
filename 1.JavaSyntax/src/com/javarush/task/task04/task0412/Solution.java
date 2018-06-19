04db4ad422e8cdd57d325f77df6c59e7c9f9f679
st_nekto <deform@tut.by>
2018-06-19 16:13:35 +0300

    the solution4012

--- 1.JavaSyntax/src/com/javarush/task/task04/task0412/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0412/Solution.java
@@ -8,7 +8,21 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        int number = Integer.parseInt(s);
+
+        if(number < 0){
+            number++;
+            System.out.println(number);
+        }
+        else if(number > 0){
+            number *= 2;
+            System.out.println(number);
+        }
+        else if(number == 0){
+            System.out.println(number);
+        }//напишите тут ваш код
 
     }
 
