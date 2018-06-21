5e4e2044b2e86bac2b4b3247c1b1ae3972b9f32d
st_nekto <deform@tut.by>
2018-06-21 16:13:43 +0300

    the solution4016

--- 1.JavaSyntax/src/com/javarush/task/task04/task0416/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0416/Solution.java
@@ -8,6 +8,17 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        double time = Double.parseDouble(s);
+
+        double x = time%5;
+
+        if(x >= 0 && x < 3)
+            System.out.println("зелёный");
+        else if(x >= 3 && x < 4)
+            System.out.println("жёлтый");
+        else
+            System.out.println("красный");//напишите тут ваш код
     }
 }
