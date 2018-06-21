0d74f1585850bb31bd34fa3b5f094fa0ebb14ee3
st_nekto <deform@tut.by>
2018-06-21 18:25:51 +0300

    the solution4018

--- 1.JavaSyntax/src/com/javarush/task/task04/task0418/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0418/Solution.java
@@ -8,6 +8,16 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        String s2 = reader.readLine();
+        int number = Integer.parseInt(s);//напишите тут ваш код
+        int number2 = Integer.parseInt(s2);
+
+        if(number > number2 || number == number2)
+            System.out.println(number2);
+        else
+            System.out.println(number);
         //напишите тут ваш код
     }
 }
