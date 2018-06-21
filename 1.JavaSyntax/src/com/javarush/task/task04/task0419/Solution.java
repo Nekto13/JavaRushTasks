d3b8eab9daf69460d34247fccff90050384c97b4
st_nekto <deform@tut.by>
2018-06-21 18:54:21 +0300

    the solution4019

--- 1.JavaSyntax/src/com/javarush/task/task04/task0419/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0419/Solution.java
@@ -8,6 +8,22 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        String s2 = reader.readLine();
+        String s3 = reader.readLine();
+        String s4 = reader.readLine();
+        int number = Integer.parseInt(s);//напишите тут ваш код
+        int number2 = Integer.parseInt(s2);
+        int number3 = Integer.parseInt(s3);
+        int number4 = Integer.parseInt(s4);
+
+        int m1 = Math.max(number, number2);
+        int m2 = Math.max(number3, number4);
+        int m3 = Math.max(m1, m2);
+        System.out.println(m3);
+
+
     }
 }
