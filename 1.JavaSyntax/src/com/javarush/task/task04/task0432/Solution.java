b7b5f614189c10839fabe1e510a3087288206732
st_nekto <deform@tut.by>
2018-07-07 19:04:47 +0300

    the solution0432

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0432/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task04.task0432;
+
+
+
+/* 
+Хорошего много не бывает
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        String s1 = reader.readLine();
+
+        int number = Integer.parseInt(s1);
+
+        int i = 0;
+
+        while(i < number){
+            System.out.println(s);
+            i++;
+        }
+
+    }
+}
