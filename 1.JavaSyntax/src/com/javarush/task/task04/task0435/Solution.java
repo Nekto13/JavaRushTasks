2878d28253f2be467026660da1cc3d8a5eb78b10
st_nekto <deform@tut.by>
2018-07-07 11:39:13 +0300

    the solution0435

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0435/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task04.task0435;
+
+/* 
+Четные числа
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int a = 1;
+        for (int i = 0; i < 100; i++) {
+            a++;
+            if(a%2 == 0)
+                System.out.println(a);
+
+        }//напишите тут ваш код
+
+    }
+}
