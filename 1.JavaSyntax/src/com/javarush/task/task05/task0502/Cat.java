11f47ff953345174a6d78b2f6942cc2e80387af1
st_nekto <deform@tut.by>
2018-07-08 19:51:27 +0300

    the solution0502

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task05.task0502;
+
+/* 
+Реализовать метод fight
+*/
+
+public class Cat {
+    public int age;
+    public int weight;
+    public int strength;
+
+    public Cat() {
+    }
+
+    public boolean fight(Cat anotherCat) {
+        if(this.age*this.weight*this.strength > anotherCat.age*anotherCat.weight*anotherCat.strength)
+            return  true;
+        else
+            return false;//напишите тут ваш код
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
