adc432cf220d8e98d046c991deafc39574fe2256
st_nekto <deform@tut.by>
2018-07-15 11:17:33 +0300

    the solution0526

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0526/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.task05.task0526;
+
+/* 
+Мужчина и женщина
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Man man1 = new Man("Tom", 20, "USA");
+        Man man2 = new Man("Din", 30, "USA");
+        Woman woman1 = new Woman("Kessi", 22, "UK");
+        Woman woman2 = new Woman("Jas", 25, "UK");
+        System.out.println(man1.toString());
+        System.out.println(man2.toString());
+        System.out.println(woman1.toString());
+        System.out.println(woman2.toString());
+        //напишите тут ваш код
+    }
+
+    //напишите тут ваш код
+    public static class Man{
+        String name;
+        int age;
+        String address;
+
+        public Man(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        @Override
+        public String toString() {
+            return name + " " + age + " " + address;
+        }
+    }
+
+    public static class Woman{
+        String name;
+        int age;
+        String address;
+
+        public Woman(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        @Override
+        public String toString() {
+            return name + " " + age + " " + address;
+        }
+    }
+}
