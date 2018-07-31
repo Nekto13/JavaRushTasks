61e14dd658faba6722b615527a9924ecff70a049
st_nekto <deform@tut.by>
2018-07-31 17:16:24 +0300

    the solution0702

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0702/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task07.task0702;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Массив из строчек в обратном порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        String[] array = new String[10];
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 8; i++) {
+            String s = reader.readLine();
+            array[i] = s;
+
+        }
+
+        for (int i = array.length - 1; i >=0 ; i--) {
+            System.out.println(array[i]);
+
+        }
+    }
+}
