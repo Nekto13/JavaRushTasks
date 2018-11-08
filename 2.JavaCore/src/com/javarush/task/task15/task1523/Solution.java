865057f809ed1da292f33e436a0871bb18c48586
st_nekto <deform@tut.by>
2018-11-08 17:59:30 +0300

    task1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public Solution() {
+    }
+
+    protected Solution(int a) {
+    }
+
+     Solution(String b) {
+    }
+
+    private Solution(short c){}
+
+    public static void main(String[] args) {
+
+    }
+}
+
