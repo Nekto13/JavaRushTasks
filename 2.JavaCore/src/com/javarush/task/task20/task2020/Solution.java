cf6c2b61431d6b68af516c43de11fa2a3c4df912
st_nekto <deform@tut.by>
2019-01-14 16:47:39 +0300

    task2020

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2020/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task20.task2020;
+
+import java.io.*;
+import java.util.logging.Logger;
+
+/* 
+Сериализация человека
+*/
+public class Solution {
+
+    public static class Person implements Serializable {
+        String firstName;
+        String lastName;
+        transient String fullName;
+        transient final String greeting;
+        String country;
+        Sex sex;
+        transient PrintStream outputStream;
+        transient Logger logger;
+
+        Person(String firstName, String lastName, String country, Sex sex) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.fullName = String.format("%s, %s", lastName, firstName);
+            this.greeting = "Hello, ";
+            this.country = country;
+            this.sex = sex;
+            this.outputStream = System.out;
+            this.logger = Logger.getLogger(String.valueOf(Person.class));
+        }
+    }
+
+    enum Sex {
+        MALE,
+        FEMALE
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        FileOutputStream fileOutput = new FileOutputStream("your.file.name");
+        ObjectOutputStream outputStream = new ObjectOutputStream(fileOutput);
+
+        Person person = new Person("Tom", "Depp", "USA", Sex.MALE);
+
+        outputStream.writeObject(person);
+
+    }
+}
