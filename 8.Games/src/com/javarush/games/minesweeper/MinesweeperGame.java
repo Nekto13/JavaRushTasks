ceea271bd4ffddaa31f62a4797ad85fa0317ae5f
st_nekto <deform@tut.by>
2019-01-16 22:14:16 +0300

    task3101

---
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -0,0 +1,11 @@
+package com.javarush.games.minesweeper;
+
+import com.javarush.engine.cell.*;
+
+public class MinesweeperGame extends Game {
+
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
+}
