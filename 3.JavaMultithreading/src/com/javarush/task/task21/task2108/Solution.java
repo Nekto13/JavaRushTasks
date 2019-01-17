98b2182ad2f5aff5e9c9c2025f01b3ecc5fd7d9b
st_nekto <deform@tut.by>
2019-01-17 19:07:41 +0300

    task3108

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2108/Solution.java
@@ -0,0 +1,71 @@
+package com.javarush.task.task21.task2108;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/*
+Клонирование растений
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Tree tree = new Tree("willow", new String[]{"s1", "s2", "s3", "s4"});
+        Tree clone = null;
+        try {
+            clone = (Tree)tree.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(tree);
+        System.out.println(clone);
+
+        System.out.println(tree.branches);
+        System.out.println(clone.branches);
+    }
+
+    public static class Plant{
+        private String name;
+
+        public Plant(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Plant plant = (Plant) o;
+            return Objects.equals(name, plant.name);
+        }
+
+        @Override
+        public int hashCode() {
+
+            return Objects.hash(name);
+        }
+    }
+
+    public static class Tree extends Plant implements Cloneable {
+        private String[] branches;
+
+        public Tree(String name, String[] branches) {
+            super(name);
+            this.branches = branches;
+        }
+
+        public String[] getBranches() {
+            return branches;
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            Tree tree = new Tree(this.getName(), Arrays.copyOf(this.branches, branches.length));
+
+            return tree;
+        }
+    }
+}
