58be0e528b1f38a3bb7e4253bd3382e58afdbd8f
st_nekto <deform@tut.by>
2019-01-18 12:17:57 +0300

    task3110

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
@@ -0,0 +1,71 @@
+package com.javarush.task.task21.task2109;
+
+import java.util.Objects;
+
+/*
+Запретить клонирование
+*/
+public class Solution {
+    public static class A implements Cloneable {
+        private int i;
+        private int j;
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            A a = (A) o;
+            return i == a.i &&
+                    j == a.j;
+        }
+
+        @Override
+        public int hashCode() {
+
+            return Objects.hash(i, j);
+        }
+    }
+
+    public static class B extends A {
+        private String name;
+
+        private B instance = new B(super.i, super.j, name);
+
+        private B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+
+        public  B getInstance(){
+            return instance;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static class C extends B implements Cloneable {
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
